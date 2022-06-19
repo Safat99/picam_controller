4e88c1199e3d0dfb90785cbcd06903b48cf5696e
Safat99 <sarkersafat99@gmail.com>
2022-06-19 13:13:31 +0300

    opencv installed on pi and working properly

---
+++ testcam.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+	print('Cannot open camera')
+	exit()
+
+while(True):
+	ret , frame = cap.read()
+	if not ret:
+		print('Can\'t receive frame....Exiting')
+		break
+
+	# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# backtorgb = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
+	# cv2.imshow('gray', gray)
+	cv2.imshow('frame',  frame)
+	# cv2.imshow('backtorgb', backtorgb)
+	if cv2.waitKey(1) ==  ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
